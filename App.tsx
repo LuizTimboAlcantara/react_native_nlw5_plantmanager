495d0580f05b5469b48b2accd3be79ffee78d58d
LuizTimboAlcantara <60242867+LuizTimboAlcantara@users.noreply.github.com>
2021-05-11 11:52:45 -0300

    Aula 02 - parte01

--- App.tsx
+++ App.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import AppLoading from "expo-app-loading";
 
-import { Welcome } from "./src/pages/Welcome";
+import { UserIdentification } from "./src/pages/UserIdentification";
+
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold,
+} from "@expo-google-fonts/jost";
 
 export default function App() {
-  return <Welcome />;
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold,
+  });
+
+  if (!fontsLoaded) return <AppLoading />;
+
+  return <UserIdentification />;
 }

9395ba6f12d7c3851b4f2c51126dbef7396afc96
LuizTimboAlcantara <joseluiztimbo@gmail.com>
2021-05-14 20:51:32 -0300

    Aula 05 - parte 02

--- src/routes/tab.routes.tsx
+++ src/routes/tab.routes.tsx
@@ -1,47 +1,41 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
 import colors from "../styles/colors";
-
-import { MaterialIcons } from "@expo/vector-icons";
-
 import { PlantSelect } from "../pages/PlantSelect";
+import { MaterialIcons } from "@expo/vector-icons";
 import { MyPlants } from "../pages/MyPlants";
+import { Platform } from "react-native";
 
-const Apptab = createBottomTabNavigator();
+const AppTab = createBottomTabNavigator();
 
 const AuthRoutes = () => {
   return (
-    <Apptab.Navigator
+    <AppTab.Navigator
       tabBarOptions={{
-        activeTintColor: colors.green,
-        inactiveTintColor: colors.heading,
+        activeBackgroundColor: colors.shape,
+        inactiveBackgroundColor: colors.shape,
         labelPosition: "beside-icon",
         style: {
-          paddingVertical: 20,
+          paddingVertical: Platform.OS == "ios" ? 20 : 0,
           height: 88,
         },
       }}
     >
-      <Apptab.Screen
+      <AppTab.Screen
         name="Nova Planta"
         component={PlantSelect}
         options={{
-          tabBarIcon: ({ size, color }) => (
-            <MaterialIcons name="add-circle-outline" size={size} color={color} />
-          ),
+          tabBarIcon: ({ size, color }) => <MaterialIcons name="add-circle-outline" size={size} color={color} />,
         }}
       />
-      <Apptab.Screen
+      <AppTab.Screen
         name="Minhas Plantas"
         component={MyPlants}
         options={{
-          tabBarIcon: ({ size, color }) => (
-            <MaterialIcons name="format-list-bulleted" size={size} color={color} />
-          ),
+          tabBarIcon: ({ size, color }) => <MaterialIcons name="format-list-bulleted" size={size} color={color} />,
         }}
       />
-    </Apptab.Navigator>
+    </AppTab.Navigator>
   );
 };
 
